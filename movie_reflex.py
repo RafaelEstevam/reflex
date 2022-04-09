79eaa0049c0779b8e9f48be8b055f958557d4aec
Rafael <rafael.estevamdesign@gmail.com>
2022-04-09 13:54:46 -0300

    Remove comment

--- movie_reflex.py
+++ movie_reflex.py
@@ -10,23 +10,11 @@ application_collection = application_db["rule"]
 composite_rules = application_collection.find()
 inferences = []
 for composite_rule in composite_rules:
-
-    # print(composite_rule)
-
     rules = []
-    # print(composite_rule['rules'])
     rule = composite_rule['rules']
-    # for rule in composite_rule['rules']:
-
     r = Rule(rule['relation'], rule['percept_ref'], rule['percept_name'], rule['action'])
     rules.append(r)
     inferences.append(Inference(rules, composite_rule['operators'], rule['action']))
-
-# percepts = [
-#     {'eccomerce_item': 'RETROSPOT HEART HOT WATER BOTTLE'},
-#     {'eccomerce_item': 'SCOTTIE DOG HOT WATER BOTTLE'},
-#     {'eccomerce_item': "HEART OF WICKER SMALL"},
-# ]
 
 item = input("Qual foi o filme assistido? \n")
 percepts = [{"movie": "'" + item + "'" }]
@@ -34,8 +22,6 @@ percepts = [{"movie": "'" + item + "'" }]
 for inference in inferences:
 
     for percept in percepts:
-
-        # print(percept.get('movie'))
 
         inference_result = inference.infer(percept)
         if inference_result != 'False':
